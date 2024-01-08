af16d4b3a34cdcf1bd1b25f9a4d36a00e02d35f2
Thouseef <thousithouseef731@gmail.com>
2024-01-08 12:10:05 +0530

    Deploying on render

--- doodle/settings.py
+++ doodle/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 import os
+import dj_database_url
 
 load_dotenv()
 
@@ -99,22 +100,19 @@ ASGI_APPLICATION = "doodle.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+DATABASES = {
+    "default":dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "DoodleDB",
+#         "USER": "postgres",
+#         "PASSWORD": "2544",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
 #     }
 # }
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "DoodleDB",
-        "USER": "postgres",
-        "PASSWORD": "2544",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
-    }
-}
 
 
 # Password validation
